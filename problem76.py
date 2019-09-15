6fdbfd6283bf11248a52d2e522434be5cbd2e0a6
abhijeetknayak <abhijeetknayak@gmail.com>
2019-09-15 12:26:33 +0530

    Add files via upload

--- problem76.py
+++ problem76.py
@@ -1,22 +1,26 @@
-str = 'a'
-
-str1 = 'b'
-
-array = ["abc", "def", "ghi"]
-
-# Function to extract column values as a list
+# Function to extract column values and check if it is valid
 def extract_col_values(array, col):
-    return [array[i][col] for i in range(0, len(array))]
+    k = 1
+    for i in range(0, len(array) - 1):
+        k = k * (array[i][col] < array[i + 1][col])
+    return k
+
 
 def get_invalid_col(array):
+    valid_col = 0
     for i in range(0, len(array[0])): # Number of Columns
-        col = extract_col_values(array, i)
-        print(col)
+        valid_col += extract_col_values(array, i)
+    return (len(array[0]) - valid_col)
 
+if __name__ == '__main__':
+    array = ["abc", "def", "ghi"]
+    print(get_invalid_col(array))
 
+    array = ["cba", "daf", "ghi"]
+    print(get_invalid_col(array))
 
-get_invalid_col(array)
+    array = ["abcdef"]
+    print(get_invalid_col(array))
 
-
-
-print(str1 > str)
+    array = ["zxy", "lmn", "abc"]
+    print(get_invalid_col(array))
